f161a67b617074ce562e90a9a3f81c70e499a9cf
Andrew Truong <adh.truong@gmail.com>
2022-09-24 13:12:52 +0100

    Add github deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProd ? "/generative-art-website/" : "",
+  /**
+   * Disable server-based image optimization.
+   *
+   * @see https://nextjs.org/blog/next-12-3#disable-image-optimization-stable
+   */
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
